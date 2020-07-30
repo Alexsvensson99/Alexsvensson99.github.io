a98d964b226a1f6350e92680abc9f503f476ec7f
Alexsvensson99 <alexandersvensson99@outlook.com>
2020-07-30 21:11:07 +0200

    Multiple changes

--- js/main.js
+++ js/main.js
@@ -23,3 +23,25 @@
     });
 
 })(jQuery);
+var element = document.querySelector('#months-between');
+$("span#months-between").each(function(i){
+    var startDate = new Date($(this).data('firstdate'));
+
+    if ($(this).data('seconddate') == 'current') {
+    var endDate = new Date();
+    } else {
+    var endDate = new Date($(this).data('seconddate'));
+    }
+
+    var endMoment   = moment(endDate);
+    var startMoment = moment(startDate);
+
+    if (endMoment.diff(startMoment, 'months') !== 1){
+        var month = " månader";
+    } else {
+        var month = " månad";
+    }
+
+    console.log(endMoment.diff(startMoment, 'months'));
+    $(this).prepend('(',endMoment.diff(startMoment, 'months'), month, ')');
+});
