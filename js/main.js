a637383b89a23208ddcc92a067ab8667b9d7ecb4
Alexsvensson99 <alexandersvensson99@outlook.com>
2020-08-06 23:57:51 +0200

    Removed strict mode

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 (function ($) {
-    "use strict";
 
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
