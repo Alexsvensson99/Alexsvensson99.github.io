b28e23d66e0d2ade9c8529ca3d00c400051481aa
Alexsvensson99 <alexandersvensson99@outlook.com>
2021-03-25 09:40:16 +0100

    Added Jessikas CV

---
+++ risberg/js/main.min.js
@@ -0,0 +1,42 @@
+$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: (target.offset().top)
+            }, 1000, "easeInOutExpo");
+            return false;
+        }
+    }
+});
+
+$('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide');
+});
+
+$('body').scrollspy({
+    target: '#sideNav'
+});
+let element = document.querySelector('#months-between');
+$("span#months-between").each(function(i){
+    let startDate = new Date($(this).data('firstdate'));
+    let endDate = '';
+    if ($(this).data('seconddate') == 'current') {
+    endDate = new Date();
+    } else {
+    endDate = new Date($(this).data('seconddate'));
+    }
+
+    let endMoment   = moment(endDate, "YYYY-MM-DD").add(1, 'M');
+    let startMoment = moment(startDate, "YYYY-MM-DD");
+    let month = '';
+    if (endMoment.diff(startMoment, 'months') !== 1){
+        month = " månader";
+    } else {
+        month = " månad";
+    }
+
+    console.log(endMoment.diff(startMoment, 'months'));
+    $(this).prepend('(',endMoment.diff(startMoment, 'months'), month, ')');
+});
