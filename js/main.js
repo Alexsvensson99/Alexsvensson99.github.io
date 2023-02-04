9da5fa4c5b8647596adfd72423134f9428b553e8
Alexander Svensson <alexandersvensson99@outlook.com>
2023-02-04 17:45:34 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -1,42 +1,51 @@
-$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            $('html, body').animate({
-                scrollTop: (target.offset().top)
-            }, 1000, "easeInOutExpo");
-            return false;
+(function () {
+    // Caching the scroll trigger element
+    const $scrollTrigger = $('a.js-scroll-trigger[href*="#"]:not([href="#"])');
+
+    // Attaching a click event handler to the scroll trigger element
+    $scrollTrigger.on('click', function () {
+        // Checking if the pathname and hostname of the current location match the target's pathname and hostname
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            // Caching the target element
+            let target = $(this.hash);
+            // If the target element does not exist, look for an element with a name attribute equal to the hash value
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // If the target element exists, animate the page to scroll to it
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
         }
-    }
-});
+        // Hiding the navbar on click
+        $('.navbar-collapse').collapse('hide');
+    });
 
-$('.js-scroll-trigger').click(function () {
-    $('.navbar-collapse').collapse('hide');
-});
+    // Initializing the scrollspy on the body element
+    $('body').scrollspy({
+        target: '#sideNav'
+    });
 
-$('body').scrollspy({
-    target: '#sideNav'
-});
-let element = document.querySelector('#months-between');
-$("span#months-between").each(function(i){
-    let startDate = new Date($(this).data('firstdate'));
-    let endDate = '';
-    if ($(this).data('seconddate') == 'current') {
-    endDate = new Date();
-    } else {
-    endDate = new Date($(this).data('seconddate'));
-    }
+    // Caching the months-between element
+    const $monthsBetween = $("span#months-between");
 
-    let endMoment   = moment(endDate, "YYYY-MM").add(1, 'M');
-    let startMoment = moment(startDate, "YYYY-MM");
-    let month = '';
-    if (endMoment.diff(startMoment, 'months') !== 1){
-        month = " månader";
-    } else {
-        month = " månad";
-    }
+    // Iterating over the months-between elements
+    $monthsBetween.each(function () {
+        // Getting the start date from the data-firstdate attribute
+        let startDate = new Date($(this).data('firstdate'));
+        // Getting the end date from the data-seconddate attribute
+        let endDate = $(this).data('seconddate') === 'current' ? new Date() : new Date($(this).data('seconddate'));
 
-    console.log(endMoment.diff(startMoment, 'months'));
-    $(this).prepend('(',endMoment.diff(startMoment, 'months'), month, ')');
-});
+        // Calculating the difference in months between the end date and start date using moment.js
+        let endMoment = moment(endDate, "YYYY-MM").add(1, 'M');
+        let startMoment = moment(startDate, "YYYY-MM");
+
+        let months = endMoment.diff(startMoment, 'months');
+        // Adding the appropriate text based on the number of months
+        let month = months !== 1 ? " månader" : " månad";
+
+        // Prepending the number of months to the element
+        $(this).prepend(`(${months}${month})`);
+    });
+})();
